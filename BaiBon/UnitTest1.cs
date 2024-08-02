033f6fbf0f108bdfa4f3c5b434a70b078bed86ca
DESKTOP-MMEUCNO\admin <trannguyen2004ns@gmail.com>
2024-08-03 00:44:48 +0700

    hai ba bon nam sau

---
+++ BaiBon/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿namespace BaiBon
+{
+    public class Tests
+    {
+        public class Calculator
+        {
+            public double CalculateAverage(List<int> numbers)
+            {
+                if (numbers == null || numbers.Count == 0)
+                {
+                    throw new ArgumentException("Danh sách không được rỗng.");
+                }
+                return numbers.Average();
+            }
+        }
+        [TestFixture]
+        public class CalculatorTests
+        {
+            private Calculator _calculator;
+
+            [SetUp]
+            public void Setup()
+            {
+                _calculator = new Calculator();
+            }
+
+            [Test]
+            public void CalculateAverage_ValidPositiveNumbers_ReturnsCorrectAverage()
+            {
+                // Arrange
+                List<int> numbers = new List<int> { 10, 20, 30, 40, 50 };
+
+                // Act
+                double result = _calculator.CalculateAverage(numbers);
+
+                // Assert
+                Assert.AreEqual(30, result);
+            }
+
+        }
+    }
+}
---
+++ BaiNam/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿namespace BaiNam
+{
+    public class Tests
+    {
+        public class ArrayHelper
+        {
+            public int GetElementAtIndex(int[] array, int index)
+            {
+                if (index < 0 || index >= array.Length)
+                {
+                    throw new IndexOutOfRangeException("Chỉ mục nằm ngoài phạm vi");
+                }
+                return array[index];
+            }
+        }
+        [TestFixture]
+        public class ArrayHelperTests
+        {
+            private ArrayHelper arrayHelper;
+
+            [SetUp]
+            public void SetUp()
+            {
+                arrayHelper = new ArrayHelper();
+            }
+
+
+            [Test]
+            public void GetElementAtIndex_ValidIndex_ReturnsElement()
+            {
+                int[] array = { 1, 2, 3, 4, 5 };
+
+                int result = arrayHelper.GetElementAtIndex(array, 2);
+                Assert.AreEqual(3, result);
+            }
+
+
+        }
+    }
+}
